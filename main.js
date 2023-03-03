5a85913c44125af557df27133d433102ad69f6ee
graffyterlaptop <alejorpo97@gmail.com>
2023-03-02 22:21:27 -0500

    añadir botones de hojas

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//agregar nueva fila a la tabla
 const tableBody = document.querySelector('.tableBody');
 const rowAdd = document.querySelector('.buttonTable')
 
@@ -60,3 +61,71 @@ function addNewRow(){
     rowNew.appendChild(celda5);
 }
 
+//cambio de pagina
+const buttonPage1 = document.querySelector('.page1');
+const buttonPage2 = document.querySelector('.page2');
+const buttonPage3 = document.querySelector('.page3');
+const hoja1 = document.querySelector('hoja1')
+const hoja2 = document.querySelector('hoja2')
+const hoja3 = document.querySelector('hoja3')
+
+buttonPage1.addEventListener('click', colorChange1);
+buttonPage1.addEventListener('click', showHoja1);
+buttonPage2.addEventListener('click', colorChange2);
+buttonPage2.addEventListener('click', showHoja2);
+buttonPage3.addEventListener('click', colorChange3);
+buttonPage3.addEventListener('click', showHoja3);
+
+function colorChange1(){
+    const HaveClassSelected = buttonPage1.classList.contains('selected');
+    if(!HaveClassSelected){
+        buttonPage1.classList.add('selected');
+        buttonPage2.classList.remove('selected');
+        buttonPage3.classList.remove('selected');
+    }  
+}
+
+function showHoja1(){
+    const isHoja1Show = hoja1.classList.contains('noSelected');
+    if(!isHoja1Show){
+        hoja1.classList.remove('noSelected');
+        hoja2.classList.add('noSelected');
+        hoja3.classList.add('noSelected');
+    }
+}
+
+
+function colorChange2(){
+    const HaveClassSelected = buttonPage2.classList.contains('selected');
+    if(!HaveClassSelected){
+        buttonPage1.classList.remove('selected');
+        buttonPage2.classList.add('selected');
+        buttonPage3.classList.remove('selected');
+    }
+}
+
+function showHoja2(){
+    const isHoja2Show = hoja1.classList.contains('noSelected');
+    if(!isHoja2Show){
+        hoja1.classList.add('noSelected');
+        hoja2.classList.remove('noSelected');
+        hoja3.classList.add('noSelected');
+    }
+}
+
+function colorChange3(){
+    const HaveClassSelected = buttonPage3.classList.contains('selected');
+    if(!HaveClassSelected){
+        buttonPage1.classList.remove('selected');
+        buttonPage2.classList.remove('selected');
+        buttonPage3.classList.add('selected');
+    }
+}
+function showHoja3(){
+    const isHoja3Show = hoja1.classList.contains('noSelected');
+    if(!isHoja3Show){
+        hoja1.classList.add('noSelected');
+        hoja2.classList.add('noSelected');
+        hoja3.classList.remove('noSelected');
+    }
+}
